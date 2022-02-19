a6a26f10568add758110059ca7ec747f6042f0a2
Mike Bolloskis <mpb0406@gmail.com>
2022-02-18 20:53:50 -0500

    registerUser COntroller function built out up to JWT - User can register and will be created in the DB

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,22 +1,63 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../Models/userModel");
+
 // @desc    Register new user
 // @route   POST /api/users
 // @access  Public
-const registerUser = (req, res) => {
-  res.json({ message: "Register User" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //Check if fields are populated
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please add all fields");
+  }
+
+  //Check if user exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User found with this email");
+  }
+
+  //Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Build User Object to be Saved
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  //Check to See if User Was Created
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "Login User" });
-};
+});
 
 // @desc    Get User Data
 // @route   GET /api/users/me
 // @access  Public
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "Get User Data" });
-};
+});
 
 module.exports = { registerUser, loginUser, getMe };
