79f19cc9976e866fee31afa39484a6bffc5bf5a5
Mike Bolloskis <mpb0406@gmail.com>
2022-02-21 20:24:05 -0500

    Completed create, update and delete goals routes. Goals can now be added by each user and after checking for authorization be updated and deleted

--- Backend/Controllers/goalControllers.js
+++ Backend/Controllers/goalControllers.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Goal = require("../Models/goalModel");
+const User = require("../Models/userModel");
 
 // @desc    Get Goals
 // @route   GET /api/goals
@@ -21,6 +22,7 @@ const createGoals = asyncHandler(async (req, res) => {
 
   const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
 
   res.status(200).json(goal);
@@ -35,6 +37,20 @@ const updateGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal Not Found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  //Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  //Check if logged user matches user on goal
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -53,6 +69,20 @@ const deleteGoal = asyncHandler(async (req, res) => {
   if (!goal) {
     res.status(400);
     throw new Error("Goal Not Found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  //Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  //Check if logged user matches user on goal
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
 
   await goal.remove();
