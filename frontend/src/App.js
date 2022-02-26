342d006422fd47e6f6aa3eb9a389aa16549fe5bf
Mike Bolloskis <mpb0406@gmail.com>
2022-02-26 15:16:28 -0500

    Completed Header Component - still needs to conditionally render options based on login status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Dashboard from "./Pages/Dashboard";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <>
       <Router>
         <div className="container">
+          <Header />
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/register" element={<Register />} />
