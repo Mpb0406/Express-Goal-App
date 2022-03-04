1a5c7c996afac001013c69bdd3a827d5110f8b27
Mike Bolloskis <mpb0406@gmail.com>
2022-03-03 21:06:26 -0500

    Created Goal Form Component and Create Goal Async Think Function and API call

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import GoalForm from "../Components/GoalForm";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -13,7 +14,16 @@ const Dashboard = () => {
     }
   }, [user, navigate]);
 
-  return <div>dashboard</div>;
+  return (
+    <>
+      <section className="heading">
+        <h1>Welcome {user && user.name}</h1>
+        <p>Goals Dashboard</p>
+      </section>
+
+      <GoalForm />
+    </>
+  );
 };
 
 export default Dashboard;
