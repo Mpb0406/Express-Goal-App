8450d218ceef90606be8ce360ac6ec30fd5a9e90
Mike Bolloskis <mpb0406@gmail.com>
2022-03-04 21:06:19 -0500

    Finished create goal state and extra reducers and added dispatch to frontend. Completed get goals redux thunk and API call and added dispatch to dashboard component - Need to display goals

--- frontend/src/Pages/Dashboard.js
+++ frontend/src/Pages/Dashboard.js
@@ -1,18 +1,38 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import GoalForm from "../Components/GoalForm";
+import Spinner from "../Components/Spinner";
+import { reset, getGoals } from "../features/Goals/goalsSlice";
 
 const Dashboard = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  );
 
   useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+
     if (!user) {
       navigate("/login");
     }
+
+    dispatch(getGoals());
+
+    return () => {
+      dispatch(reset());
+    };
   }, [user, navigate]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
